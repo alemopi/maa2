ac5ee31e8e8e2f3577a01c5e97432939b28e8f32
alemopi <moslesar@gmail.com>
2023-03-25 15:27:46 +0300

    update2  build for gh-pages

--- maa1/src/App.js
+++ maa1/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn dfdfdfdff
+          dfdfdfdf dfdfdfdff
         </a>
       </header>
     </div>
